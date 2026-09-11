b80af2f7dfeadbf9250d683071f13efbfc24435e
psicanalistaadilsoncosta-boop <psicanalistaadilsoncosta@gmail.com>
2026-09-11 09:53:05 -0300

    004

--- app/(protocolo)/aguardando/page.jsx
+++ app/(protocolo)/aguardando/page.jsx
@@ -113,7 +113,7 @@ export default function AguardandoPage() {
               }}
             >
               Preciso reagendar
-            <            </button>
+                      </button>
           </div>
           )}
         </div>
